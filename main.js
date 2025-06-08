d691d6e66530728692cf73546af7d007706832c3
Dominic Prior <dominicprior42@gmail.com>
2025-06-08 10:09:02 +0100

    Import items individually

--- main.js
+++ main.js
@@ -4,24 +4,27 @@
 // Without the importmap (or using a bundler like Rollup.js), you would need
 // to use a relative path or a CDN URL, such as:
 // import * as THREE from "https://cdn.jsdelivr.net/npm/three@0.177.0/build/three.module.js";
-import * as THREE from 'three';
-const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
-const renderer = new THREE.WebGLRenderer();
+import {
+    Scene, PerspectiveCamera, WebGLRenderer, BoxGeometry, MeshBasicMaterial,
+    Mesh, LineBasicMaterial, Line, BufferGeometry, Vector3,
+} from 'three';
+const scene = new Scene();
+const camera = new PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+const renderer = new WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
-const cubeGeometry = new THREE.BoxGeometry( 1, 1, 1 );
-const cubeMaterial = new THREE.MeshBasicMaterial( { color: 0x00ffa0 } );
-const lineMaterial = new THREE.LineBasicMaterial( { color: 0x0000ff } );
-const cube = new THREE.Mesh( cubeGeometry, cubeMaterial );
+const cubeGeometry = new BoxGeometry( 1, 1, 1 );
+const cubeMaterial = new MeshBasicMaterial( { color: 0x00ffa0 } );
+const lineMaterial = new LineBasicMaterial( { color: 0x0000ff } );
+const cube = new Mesh( cubeGeometry, cubeMaterial );
 scene.add( cube );
 camera.position.z = 5;
 const points = [];
-points.push( new THREE.Vector3( - 1, 0, 0 ) );
-points.push( new THREE.Vector3( 0, 1, 0 ) );
-points.push( new THREE.Vector3( 1, 0, 0 ) );
-const lineGeometry = new THREE.BufferGeometry().setFromPoints( points );
-const line = new THREE.Line( lineGeometry, lineMaterial );
+points.push( new Vector3( - 1, 0, 0 ) );
+points.push( new Vector3( 0, 1, 0 ) );
+points.push( new Vector3( 1, 0, 0 ) );
+const lineGeometry = new BufferGeometry().setFromPoints( points );
+const line = new Line( lineGeometry, lineMaterial );
 scene.add( line );
 function animate() {
     requestAnimationFrame( animate );
