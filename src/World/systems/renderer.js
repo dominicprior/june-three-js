279e35ce3cffe2dcbe44814d6f3cab8924a5a256
Dominic Prior <dominicprior42@gmail.com>
2025-06-08 14:45:11 +0100

    Add physicallyCorrectLights

--- src/World/systems/renderer.js
+++ src/World/systems/renderer.js
@@ -2,7 +2,7 @@ import { WebGLRenderer } from 'https://cdn.jsdelivr.net/npm/three@0.177.0/build/
 
 function createRenderer() {
   const renderer = new WebGLRenderer();
-
+  renderer.physicallyCorrectLights = true;
   return renderer;
 }
 
