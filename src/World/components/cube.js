a7358b9fec93a0e68e8e6b3401c9505c330cd23a
Dominic Prior <dominicprior42@gmail.com>
2025-06-08 15:19:32 +0100

    Switch to a MeshStandardMaterial

--- src/World/components/cube.js
+++ src/World/components/cube.js
@@ -1,15 +1,13 @@
-import { BoxGeometry, Mesh, MeshBasicMaterial } from 'three';
+import { BoxGeometry, Mesh, MeshStandardMaterial } from 'three';
 
 function createCube() {
-  // create a geometry
   const geometry = new BoxGeometry(2, 2, 2);
-
-  // create a default (white) Basic material
-  const material = new MeshBasicMaterial();
-
-  // create a Mesh containing the geometry and material
+  const spec = {
+    color: 'purple',
+  };
+  const material = new MeshStandardMaterial(spec);
   const cube = new Mesh(geometry, material);
-
+  cube.rotation.set(-0.5, -0.1, 0.8);
   return cube;
 }
 
