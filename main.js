13fa91497deb1f4e2a232bfcda7b4f6d37fa559d
Dominic Prior <dominicprior42@gmail.com>
2025-06-08 09:45:51 +0100

    Add a comment

--- main.js
+++ main.js
@@ -1,3 +1,9 @@
+// I am following https://discoverthreejs.com/
+
+// This import statement works because of the importmap defined in index.html.
+// Without the importmap (or using a bundler like Rollup.js), you would need
+// to use a relative path or a CDN URL, such as:
+// import * as THREE from "https://cdn.jsdelivr.net/npm/three@0.177.0/build/three.module.js";
 import * as THREE from 'three';
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
