2f8e015dfd20a3533714bb34aa1f72a30f00f24d
Dominic Prior <dominicprior42@gmail.com>
2025-06-09 14:47:45 +0100

    Add a texture to the cube

--- src/world/components/cube.js
+++ src/world/components/cube.js
@@ -1,11 +1,21 @@
-import { BoxGeometry, Mesh, MeshStandardMaterial, MathUtils } from 'three';
+import { BoxGeometry, Mesh, MeshStandardMaterial, MathUtils, TextureLoader } from 'three';
+
+function createMaterial() {
+  const textureLoader = new TextureLoader();
+  const texture = textureLoader.load('assets/textures/uv-test-col.png');
+  const spec = {
+    map: texture,
+    // color: 'purple',
+  };
+  return new MeshStandardMaterial(spec);
+}
 
 function createCube() {
   const geometry = new BoxGeometry(2, 2, 2);
   const spec = {
     color: 'purple',
   };
-  const material = new MeshStandardMaterial(spec);
+  const material = createMaterial(spec);
   const cube = new Mesh(geometry, material);
   cube.rotation.set(-0.5, -0.1, 0.8);
   const radiansPerSecond = MathUtils.degToRad(30);
