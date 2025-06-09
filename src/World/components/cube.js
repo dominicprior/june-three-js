c8bad68f5a1ca173217e1f472e880418782fa0d7
Dominic Prior <dominicprior42@gmail.com>
2025-06-09 10:16:18 +0100

    An an animation loop

--- src/World/components/cube.js
+++ src/World/components/cube.js
@@ -1,4 +1,4 @@
-import { BoxGeometry, Mesh, MeshStandardMaterial } from 'three';
+import { BoxGeometry, Mesh, MeshStandardMaterial, MathUtils } from 'three';
 
 function createCube() {
   const geometry = new BoxGeometry(2, 2, 2);
@@ -8,6 +8,11 @@ function createCube() {
   const material = new MeshStandardMaterial(spec);
   const cube = new Mesh(geometry, material);
   cube.rotation.set(-0.5, -0.1, 0.8);
+  const radiansPerSecond = MathUtils.degToRad(30);
+  cube.tick = (delta) => {
+    cube.rotation.y += radiansPerSecond * delta;
+  };
+
   return cube;
 }
 
--- src/main.js
+++ src/main.js
@@ -2,14 +2,9 @@ console.log('dominic!');
 import { World } from './World/World.js';
 
 function main() {
-  // Get a reference to the container element
   const container = document.querySelector('#scene-container');
-
-  // create a new world
   const world = new World(container);
-
-  // draw the scene
-  world.render();
+  world.start();
 }
 
 main();
